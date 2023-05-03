09c4ff95d728c7594eaefac3e3db79d8d2a308c1
KrishnaRSutariya <krishnar.sutariyarskd154@gmail.com>
2023-05-03 15:08:03 +0530

    Second Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require('cors');
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://long-cowboy-boots-jay.cyclic.app', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
